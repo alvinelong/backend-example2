df201af6f68ebbbc4f2a0b447992227dc67b8202
Alvin Leong <leonghongrui@gmail.com>
2022-02-28 12:46:30 +0800

    include html

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 // import the express library
 const express = require("express");
 
+// import cors to allow running backend on same machine as frontend.
+const cors = require("cors");
+
 // import router from routers.js
 const { router } = require("./routers");
 
 // define server instance
 let app = express();
+app.use(cors());
+app.use(express.json());
 
 // tell the app to use router for API mappings
 app.use(router);
 
 // start the server at port 3000
-app.listen(3000, (errors) => {
+app.listen(process.env.PORT || 3000, (errors) => {
   if (errors) {
     console.log(errors);
   } else {
